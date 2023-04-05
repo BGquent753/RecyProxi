882d0f3130efc36e740671c3565c5445a7c49666
BGquent753 <quentin.maitre753@gmail.com>
2023-04-05 11:16:48 +0200

    gitignore + test methods

--- RecyProxiAPI.js
+++ RecyProxiAPI.js
@@ -4,3 +4,32 @@ const bp = require('body-parser')
 
 const app = express();
 const port = 3000
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+//users & admins
+app.get("/", (req, res) => {
+    //recupère les centres les plus proches qui contiennent les filtres spécifiés
+    res.send("methode get");
+})
+
+//admins
+app.post("/", (req, res)=>{
+    //ajouter un centre
+    res.send("methode post");
+})
+
+app.delete("/", (req, res) =>{
+    //supprimer un centre
+    res.send("methode delete");
+})
+
+app.patch("/", (req, res) => {
+    //modifier un centre
+    res.send("methode patch");
+})
+
+app.listen(port, () => {
+    console.log('Server is running at port 3000');
+});
